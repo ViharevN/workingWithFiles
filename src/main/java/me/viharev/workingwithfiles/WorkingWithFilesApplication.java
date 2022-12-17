552f34dcf4ba3ad1eaa4842fc275fe03696395a2
ViharevN <swix_off@mail.ru>
2022-12-17 17:24:46 +0300

    commit

---
+++ src/main/java/me/viharev/workingwithfiles/WorkingWithFilesApplication.java
@@ -0,0 +1,13 @@
+package me.viharev.workingwithfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingWithFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkingWithFilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/viharev/workingwithfiles/WorkingWithFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.viharev.workingwithfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingWithFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
